1b4e199dc901e5113ce3be65d9b3832246bda9cc
Thijs Molendijk <info@thijsmolendijk.nl>
2019-09-09 20:27:42 +0200

    Huge blob of initial layout CSS

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import "./fonts.styl";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount("#app");
